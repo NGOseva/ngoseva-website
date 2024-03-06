f693eadbafe2f273ae485f9818b0ff32e6f21d43
MohitDhaker07 <mohit.dhaker04@gmail.com>
2024-03-07 02:46:11 +0530

    chat-bot-update

--- script-chatbot.js
+++ script-chatbot.js
@@ -5,7 +5,7 @@ const chatInput = document.querySelector(".chat-input textarea");
 const sendChatBtn = document.querySelector(".chat-input span");
 
 let userMessage = null; // Variable to store user's message
-const API_KEY = "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"; // Paste your API key here
+const API_KEY = ""; // Paste your API key here
 const inputInitHeight = chatInput.scrollHeight;
 
 const createChatLi = (message, className) => {
